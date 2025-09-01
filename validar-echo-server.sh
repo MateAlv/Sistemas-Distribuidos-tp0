fcd0c9ce8fae8eeeb54ad3a56fa87055cee78c33
Mate <mateoalvarez2012@gmail.com>
2025-09-01 11:09:55 -0300

    Ej3 script

---
+++ validar-echo-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SERVER_PORT = 12345
+SERVER_IP = server
+SERVER_LISTEN_BACKLOG = 5
+LOGGING_LEVEL = INFO
+NETWORK_NAME="tp0_testing_net"
+MESSAGE="Hola Mate"
+
+RESPONSE=$(docker run --rm --network=${NETWORK_NAME} appropriate/nc ${SERVER_IP} ${SERVER_PORT} <<< "${MESSAGE}")
+
+if [[ "${RESPONSE}" == "${MESSAGE}" ]]; then
+  echo "action: test_echo_server | result: success"
+else
+  echo "action: test_echo_server | result: fail"
+fi
